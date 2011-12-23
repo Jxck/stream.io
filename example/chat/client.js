68b6b49801c89704e94ef7824f25b47ebe1c1eda
Jxck <block.rxckin.beats@gmail.com>
2011-12-24 00:49:29 +0900

    add chat sample

---
+++ example/chat/client.js
@@ -0,0 +1,14 @@
+var socket = io.connect();
+
+$(function() {
+  $('#ok').click(function() {
+    var msg = $('#msg').val();
+    socket.emit('msg send', msg);
+  });
+});
+
+socket.on('msg push', function(data) {
+  console.log(data);
+  $li = $('<li>').text(data);
+  $('#display').append($li);
+});
---
+++ example/chat/server.js
@@ -0,0 +1,35 @@
+// Server
+var log = console.log.bind(console);
+var io = require('socket.io')
+  , connect = require('connect')
+  , fs = require('fs')
+  , util = require('util')
+  , stream = require('stream');
+
+
+var ReadLineFilter = require('./readLineFilter')
+  , ClientStream = require('../clientStream')
+  ;
+
+var server = connect.createServer(
+    connect.logger()
+  , connect.static(__dirname)
+).listen(3000);
+
+io = io.listen(server);
+ 
+io.configure('development', function() {
+  io.set('log level', 1);
+  io.set('transports', ['websocket']);
+});
+
+io.sockets.on('connection', function(socket) {
+  log('connected');
+  var readable = fs.createReadStream('sample.log', {encoding: 'utf-8'})
+    , readline = new ReadLineFilter()
+    , client = new ClientStream(socket)
+    ;
+  readable.resume();
+  readable.pipe(readline).pipe(client);
+});
+
