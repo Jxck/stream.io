8469a0583a89815dccc38fc1dd15e5d42f09cf46
Jxck <block.rxckin.beats@gmail.com>
2012-06-21 00:11:59 +0900

    update template doc

--- template/readableStream.js
+++ template/readableStream.js
@@ -6,6 +6,8 @@ function ReadableStream() {
   /**
    * A boolean that is true by default, but turns false after an 'error' occurred,
    * the stream came to an 'end', or destroy() was called.
+   *
+   * デフォルト True で 'error', 'end', destroy() で false になる。
    */
   this.readable = true;
   this.encoding = 'utf8';
@@ -17,6 +19,9 @@ util.inherits(ReadableStream, Stream);
 /**
  * Makes the data event emit a string instead of a Buffer.
  * encoding can be 'utf8', 'ascii', or 'base64'.
+ *
+ * Buffer ではなく string として emit する場合に使う encode 指定。
+ * 'utf8', 'ascii', 'base64' のいずれか。
  */
 ReadableStream.prototype.setEncoding = function(encoding) {
   this.encoding = encoding;
@@ -24,6 +29,8 @@ ReadableStream.prototype.setEncoding = function(encoding) {
 
 /**
  * Pauses the incoming 'data' events.
+ *
+ * 'data' イベントの発生を止める(pause)。
  */
 ReadableStream.prototype.pause = function() {
 
@@ -31,6 +38,8 @@ ReadableStream.prototype.pause = function() {
 
 /**
  * Resumes the incoming 'data' events after a pause()
+ *
+ * pause() の後に 'data' イベントを再会する。
  */
 ReadableStream.prototype.resume = function() {
 
@@ -39,6 +48,9 @@ ReadableStream.prototype.resume = function() {
 /**
  * Closes the underlying file descriptor.
  * Stream.Prototype will not emit any more events.
+ *
+ * file descriptor を close する。
+ * これ以降 stream ではどんなイベントも発生しない。
  */
 ReadableStream.prototype.destroy = function() {
   this.readable = false;
@@ -46,6 +58,8 @@ ReadableStream.prototype.destroy = function() {
 
 /**
  * After the write queue is drained, close the file descriptor.
+ *
+ * 書き出しのキューが空になってから destroy()
  */
 ReadableStream.prototype.destroySoon = function() {
 
@@ -53,6 +67,8 @@ ReadableStream.prototype.destroySoon = function() {
 
 /**
  * This is a Stream.prototype method available on all Streams.
+ *
+ * Stream.prototype に実装されているから、継承するだけ。
  */
 ReadableStream.prototype.pipe = function(writable /*, [options]*/) {
   Stream.prototype.pipe.apply(this, arguments);
@@ -65,6 +81,8 @@ var readable = new ReadableStream();
 /**
  * Event: 'data'
  * The 'data' event emits either a Buffer (by default) or a string if setEncoding() was used.
+ *
+ * イベント発生時は Buffer(デフォルト) か string が使われる。
  */
 readable.on('data', function(data) {
 
@@ -75,6 +93,10 @@ readable.on('data', function(data) {
  * Emitted when the stream has received an EOF (FIN in TCP terminology).
  * Indicates that no more 'data' events will happen.
  * If the stream is also writable, it may be possible to continue writing.
+ *
+ * stream が EOF, FIN などで終わった時 emit される。
+ * これ以上 'data' イベントは発生しない。
+ * writable だった場合、書き込みは可能。
  */
 readable.on('end', function() {
   readable.readable = false;
@@ -83,6 +105,8 @@ readable.on('end', function() {
 /**
  * Event: 'error'
  * Emitted if there was an error receiving data.
+ *
+ * エラーが発生した時。
  */
 readable.on('error', function(exception) {
   readable.readable = false;
@@ -93,6 +117,9 @@ readable.on('error', function(exception) {
  * Emitted when the underlying file descriptor has been closed.
  * Not all streams will emit this.
  * (For example, an incoming HTTP request will not emit 'close'.)
+ *
+ * file descriptor などが close された時。
+ * 全ての stream で発生するとは限らない。(HTTP リクエストなど)
  */
 readable.on('close', function() {
 
--- template/writableStream.js
+++ template/writableStream.js
@@ -6,6 +6,8 @@ function WritableStream() {
   /**
    * A boolean that is true by default, but turns false after an 'error' occurred
    * or end() / destroy() was called.
+   *
+   * デフォルト true. 'error', end(), destroy() で false になる。
    */
   this.writable = true;
 }
@@ -21,11 +23,25 @@ util.inherits(WritableStream, stream.Stream);
  * The 'drain' event will indicate when the kernel buffer is empty again.
  * The encoding defaults to 'utf8'.
  *
+ * 与えられた encoding で文字列を stream に書き出す。
+ * カーネルバッファに書きだされたら true を、
+ * バッファが一杯で書き出せなかった場合 false を返す。
+ * 書き出せなかった場合は、いずれ書き出される。
+ * 'drain' イベントはバッファが再度空になったら発生する。
+ * encoding のデフォルトは 'utf8'
+ *
  * If the optional fd parameter is specified,
  * it is interpreted as an integral file descriptor to be sent over the stream.
  * This is only supported for UNIX streams, and is silently ignored otherwise.
  * When writing a file descriptor in this manner,
  * closing the descriptor before the stream drains risks sending an invalid (closed) FD.
+ *
+ * オプションで fd が指定された場合、
+ * 書き込み対象の stream として利用される。
+ * これは UNIX でのみサポートされ、他では無視されます。
+ * この方法で file descriptor に書き込んだ場合
+ * stream が drain する前に fd を閉じると、無効な fd に書き込むおそれがある。
+ *
  */
 // WritableStream.prototype.write(buffer) {
 WritableStream.prototype.write = function(data, encoding /*, [fd]*/) {
@@ -43,6 +59,11 @@ WritableStream.prototype.write = function(data, encoding /*, [fd]*/) {
  * This call will allow queued write data to be sent before closing the stream.
  * function() or function(string, encoding) or function(buffer);
  * end with string or buffer is useful to reduce the number of packets sent.
+ *
+ * Stream を EOF, FIN で終わらせる。
+ * Stream が閉じる前に、キューに残ったデータを送ることができる。
+ * 引数は、なし, String, buffer を選択でき、 string か buffer なら
+ * パケット数を減らせる。
  */
 WritableStream.prototype.end = function() {
   this.writable = false;
@@ -51,6 +72,10 @@ WritableStream.prototype.end = function() {
 /**
  * Closes the underlying file descriptor.
  * Stream will not emit any more events. Any queued write data will not be sent.
+ *
+ * file descriptor を閉じる。
+ * Stream はこれ以上イベントを発生しない。
+ * queue のデータもこれ以上送られない。
  */
 WritableStream.prototype.destroy = function() {
   this.writable = false;
@@ -60,6 +85,9 @@ WritableStream.prototype.destroy = function() {
  * After the write queue is drained, close the file descriptor.
  * destroySoon() can still destroy straight away,
  * as long as there is no data left in the queue for writes.
+ *
+ * write のキューが空になったら、file descriptor を閉じる。
+ * キューが空ならすぐ終わる。
  */
 WritableStream.prototype.destroySoon = function() {
 
@@ -73,6 +101,9 @@ var writable = new WritableStream();
  * Event: 'drain'
  * After a write() method returned false,
  * this event is emitted to indicate that it is safe to write again.
+ *
+ * wrtite() が false を返した後に発生。
+ * 再度書き込みが可能なことを知らせる。
  */
 writable.on('drain', function() {
 
@@ -81,6 +112,8 @@ writable.on('drain', function() {
 /**
  * Event: 'error'
  * Emitted on error with the exception exception.
+ *
+ * エラー発生時に、コールバックに exception を渡す。
  */
 writable.on('error', function(exception) {
   writable.writable = false;
@@ -89,6 +122,8 @@ writable.on('error', function(exception) {
 /**
  * Event: 'close'
  * Emitted when the underlying file descriptor has been closed.
+ *
+ * file descriptor が close した時に発生
  */
 writable.on('close', function() {
 
@@ -97,6 +132,8 @@ writable.on('close', function() {
 /**
  * Event: 'pipe'
  * Emitted when the stream is passed to a readable stream's pipe method.
+ *
+ * 自身が readable stream の pipe() に渡された時に発生。
  */
 writable.on('pipe', function(reabable) {
 
